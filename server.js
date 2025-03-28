1299985863f6b6686d69ae02dcf941744e40e9f5
scclairec <b2028scacoloma@pshs.edu.ph>
2025-03-28 04:21:34 +0000

    Pending changes exported from your codespace

--- server.js
+++ server.js
@@ -22,6 +22,60 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+app.get("/happy", (req, res) => {
+  res.render("happy");
+});
+
+app.post("/happy", (req, res) => {
+  let gender = req.body.gender;
+  let name = req.body.name;
+  const output = []; //array
+  let pronouns = "she's"; //initial pronoun
+  const people = [];
+  const message = [];
+
+  console.log(req.body);
+
+  if (name == "" || gender == "") {
+    output.push("Error, please fill out your details!");
+  } //check if name and gender have values
+
+  if (gender == "Male") {
+    pronouns = "he's"; //change initial pronoun if gender is male
+  }
+
+  const lyrics = [
+    "Happy", "birthday", "to", "you!", 
+    "Happy", "birthday", "to", "you!",
+    "Happy", "birthday", "dear", `${name},`,
+    "Happy", "birthday", "to", "you!",
+    `For ${pronouns} a jolly good fellow, 
+    For ${pronouns} a jolly good fellow, 
+    For ${pronouns} a jolly good fellow, 
+    "That nobody can deny!"`
+  ]; //last 4 lines are merged for a cleaner output
+
+  for (let i = 1; req.body[`name${i}`]; i++){
+    if (req.body[`checkbox${i}`] == "on"){ 
+      people.push(req.body[`name${i}`]); //if checkbox == "on" it includes it in the people singing
+    }
+  }
+
+  console.log(people);
+
+  if (people.length > 0){
+    for (let z = 0; z < lyrics.length; z++) {
+      let person = people[z % people.length]; //so the names are looped properly 
+      output.push(`${person}: ${lyrics[z]}`); //name matching with corresponding lyric
+    }
+  }
+    
+  console.log(output);
+  
+  res.render("happy", {finalOutput: output.join('\n')});
+
+});
+
 // Create express route binder for draw.hbs and get the data from the url as parameters
 // that came from index.hbs
 
